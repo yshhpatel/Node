920f6bcedc7f260e7ddd8379fd9cf4c5ac601943
yshhpatel <162433826+yshhpatel@users.noreply.github.com>
2024-12-16 13:44:49 +0530

    Add files via upload

---
+++ Day-4/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const port = 2003
+
+const app = express()
+
+let Book = []
+app.set('view engine','ejs')
+app.use(express.urlencoded())
+
+app.get('/',(req,res)=>{
+    res.render("index",{Book})
+})
+app.post("/addData",(req,res)=>{
+    // console.log(req.body)
+    req.body.id = String(Date.now()) 
+    Book.push(req.body)
+    res.redirect("/")
+
+
+})
+app.get("/deleteData",(req,res)=>{
+    // console.log(req.query.id)
+    Book = Book.filter((e)=>e.id !== req.query.id)
+    res.redirect("/")
+})
+app.get("/editData",(req,res)=>{
+    // console.log(req.query.id)
+    singleBook = Book.find((e)=>e.id == req.query.id)
+    res.render("update",{singleBook});
+})
+app.post("/updateData",(req,res)=>{
+    let tempData = Book.map((e)=>e.id == req.body.id?req.body: e)
+    Book = tempData
+    res.redirect("/")
+})
+
+app.listen(port,(err)=>{
+    err?console.log(err):console.log(`server started http://localhost:${port}/`)
+})
